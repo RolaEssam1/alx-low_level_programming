07861d5494c22491242e72a87bd3bf437079c093
rola <rolaessam03@gmail.com>
2023-07-18 09:30:23 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphapet - Make the alphapet
+ * Return: void
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
